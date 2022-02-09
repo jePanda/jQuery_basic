fcfa9abf041880acfd03f9a1eea704a3f29e58f5
jePanda <71878529+jePanda@users.noreply.github.com>
2022-02-09 22:31:15 +0100

    Update isbn-check.php
    added comments

--- isbn-check.php
+++ isbn-check.php
@@ -10,9 +10,13 @@
     $( document ).ready(function()
     {
         console.log("Ready");
-
+        /* über die Click-Action wird die Berechnung aufgerufen */
         $("#btnGetPruefz").click(function(){
+            
+             /* originale Eingabe wird beibehalten */
             var originISBN =  $('#isbnOhneP').val();
+            
+            /*zu Testzwecken wird die Länge ausgegeben */
             alert('Origin: ' + originISBN.length);
             var isbn = originISBN.replace(/-/g, '');
 
@@ -21,17 +25,21 @@
             var sum=0;
             var pruefz=0
             var i=1;
-
+            
+            /* eingetragene ISBN wird auf Länge geprüft */
             if (isbn.length != 9) {
                 alert("ISBN ohne Prüfziffer muss 9 Stellen lang sein");
                 originISBN = 'ISBN ohne Prüfziffer muss 9 Stellen lang sein';
                 pruefz ='';
             }
             else {
-
+                
+                /* isbn wird gesplitet (sieht im array so aus 1,2,3,4) */
                 var eachNum = isbn.toString().split('').map(iNum => parseInt(iNum, 10));
 
                 for (i; i <=isbn.length; i++) {
+                    
+                    /* jede Nummer im Array wird mit dem aktuellen Index multipliziert (array starten bei 1) */
                     sum += eachNum[i-1] * i;
                     //alert('sum: '+ sum + ' eachNum: '+ eachNum[i-1] + (i));
                 }
@@ -45,6 +53,7 @@
             var $input = $("<input name='isbnMitPruef'>").text(originISBN+'-' +pruefz);
                 $input.val(originISBN+'-' +pruefz);
 
+            /* Label und Inputfeld werden dem DOM-Element "Div" bzw. der entspr. ID hinzugefügt*/
             $('#isbnArea').append('<br><label for="isbnMitPruef">ISBN mit Pruefziffer: </label>');
             $('#isbnArea').append($input);
         });
